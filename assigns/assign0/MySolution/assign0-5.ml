55e3d62f3d6ab5483272a9dbd35fedc29d7a5ee5
keith <keithy0157@gmail.com>
2023-09-12 11:19:13 -0400

    created assign4 and assign5 files

---
+++ assigns/assign0/MySolution/assign0-5.ml
@@ -0,0 +1,9 @@
+(*
+Assign0-5: 10 points
+Please implement a function that returns the reverse of
+a given string:
+fun stringrev(cs: string): string
+Note that you are not allowed to use string concatenation
+or your solution is disqualified.
+*)
+
