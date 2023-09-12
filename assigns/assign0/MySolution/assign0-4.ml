c757443b7c81223c40710da0756fbc22d78240e7
keith <keithy0157@gmail.com>
2023-09-12 13:59:29 -0400

    added comments and the working code for assign4

--- assigns/assign0/MySolution/assign0-4.ml
+++ assigns/assign0/MySolution/assign0-4.ml
@@ -9,3 +9,41 @@ for natural numbers x.
 (And the empty sequence represents the integer 0)
 *)
 
+(* Define the ord function to get the ASCII code of a character *)
+let ord = Char.code
+
+(* Define the string_length function to get the length of a string *)
+let string_length = String.length
+
+(* Define the string_get function to retrieve a character from a string at a specific index *)
+let string_get cs i0 = String.get cs i0
+
+let str2int (cs: string): int =
+  let len = string_length cs in
+
+  (* Define a recursive helper function to convert the string to an integer *)
+  let rec convert_to_int i result =
+    (* Base case: If the current index is beyond the string length, return the accumulated result *)
+    if i >= len then
+      result
+    else
+      (* Extract the character at the current index, convert it to a digit value,
+         and update the result by multiplying it by 10 and adding the digit value *)
+      let digit_value = ord (string_get cs i) - ord '0' in
+      let updated_result = result * 10 + digit_value in
+      (* Recursive call to process the next character in the string *)
+      convert_to_int (i + 1) updated_result
+  in
+
+  (* Check if the input string is empty (length is 0) *)
+  if len = 0 then
+    0 (* If empty, return 0 as specified in the prompt *)
+  else
+    (* Start the conversion process from the beginning of the string with an initial result of 0 *)
+    convert_to_int 0 0
+  ;;
+
+
+
+
+
