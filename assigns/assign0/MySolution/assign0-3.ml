9624be32b0166ecd4700669743d1e593b027edfc
keith <keithy0157@gmail.com>
2023-09-12 02:23:17 -0400

    completed and tested my code for assign3

--- assigns/assign0/MySolution/assign0-3.ml
+++ assigns/assign0/MySolution/assign0-3.ml
@@ -5,3 +5,38 @@ integer to a string that represents the integer:
 fun int2str(i0: int): string
 *)
 
+
+let chr = Char.chr
+(* Define a character-to-integer conversion function *)
+
+let string_init = String.init
+(* Define a function for initializing a string *)
+
+
+(*
+   int2str: Takes an integer i0 and returns a string representation of i0.
+   It uses a recursive function to generate the string representation.
+*)
+let int2str i0 =
+  let rec count_digits i count =
+    if i < 10 then
+      count + 1
+    else
+      count_digits (i / 10) (count + 1)
+  in
+
+  let num_digits = count_digits i0 0 in
+
+  let str = string_init num_digits (fun i ->
+    let digit_value = (i0 / int_of_float (10. ** float_of_int (num_digits - i - 1))) mod 10 in
+    char_of_int (48 + digit_value)
+  ) in
+  
+  str
+
+
+
+
+
+
+
