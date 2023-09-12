1c3f21eb59511edef296455c1fa689218ab3aaae
keith <keithy0157@gmail.com>
2023-09-12 12:24:21 -0400

    wrote the code and added comments to each function

--- assigns/assign0/MySolution/assign0-5.ml
+++ assigns/assign0/MySolution/assign0-5.ml
@@ -7,3 +7,19 @@ Note that you are not allowed to use string concatenation
 or your solution is disqualified.
 *)
 
+let string_init = String.init
+(*function is used to generate the reversed string by fetching characters from cs in reverse order and placing them in result*)
+let string_length = String.length
+let string_get = String.get
+(*used to retrieve the character at a specific index within a given string*)
+
+let stringrev(cs: string): string =
+  let len = string_length cs in
+  (* calculates the length of the input string cs.*)
+
+  let result = string_init len (fun i ->
+    (*creates an empty string result of the same length as cs*)
+    string_get cs (len - 1 - i)
+  ) in
+
+  result
