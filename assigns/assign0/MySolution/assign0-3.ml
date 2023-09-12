5644587b010d02d899d44afbc7fd85952c83df77
keith <keithy0157@gmail.com>
2023-09-12 02:25:10 -0400

    added and fixed the comments for the code

--- assigns/assign0/MySolution/assign0-3.ml
+++ assigns/assign0/MySolution/assign0-3.ml
@@ -15,10 +15,10 @@ let string_init = String.init
 
 (*
    int2str: Takes an integer i0 and returns a string representation of i0.
-   It uses a recursive function to generate the string representation.
 *)
 let int2str i0 =
   let rec count_digits i count =
+    (* Helper function to count the number of digits in an integer *)
     if i < 10 then
       count + 1
     else
@@ -26,12 +26,18 @@ let int2str i0 =
   in
 
   let num_digits = count_digits i0 0 in
+  (* Calculate the number of digits in the input integer i0 *)
+
 
   let str = string_init num_digits (fun i ->
+  (* Calculate the digit value at position i *)
+
     let digit_value = (i0 / int_of_float (10. ** float_of_int (num_digits - i - 1))) mod 10 in
+    (* Convert the digit value to a character and add to the string *)
+    
     char_of_int (48 + digit_value)
   ) in
-  
+
   str
 
 
