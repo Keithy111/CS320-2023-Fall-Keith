597f5012012405b4ea0ec7d3aa050a4c69796ac1
keith <keithy0157@gmail.com>
2023-09-13 16:30:54 -0400

    added the negative case for assign3

--- assigns/assign0/MySolution/assign0-3.ml
+++ assigns/assign0/MySolution/assign0-3.ml
@@ -28,21 +28,27 @@ let int2str i0 =
       if acc = "" then "0" else acc
     else
       (* Calculate the remainder when dividing by 10 to get the last digit *)
-      let remainder = num mod 10 in
+      let remainder = abs num mod 10 in
       (* Convert the digit to a character *)
-      let digit_char = chr (ord '0' + remainder) in
+      let digit_char = char_of_int (int_of_char '0' + remainder) in
       (* Convert the character back to a string *)
-      let digit_str = str digit_char in
+      let digit_str = String.make 1 digit_char in
       (* Calculate the length of the new accumulated string *)
       let new_acc_length = String.length digit_str + String.length acc in
       (* Create the new accumulated string by interleaving characters from digit_str and acc *)
-      let new_acc = string_init new_acc_length (fun i ->
+      let new_acc = String.init new_acc_length (fun i ->
         if i < String.length digit_str then
-          string_get digit_str i
+          String.get digit_str i
         else
-          string_get acc (i - String.length digit_str)
+          String.get acc (i - String.length digit_str)
       ) in
       (* Recursively convert the remaining part of the number *)
       int_to_str_aux (num / 10) new_acc
   in
-  int_to_str_aux i0 ""
+  if i0 < 0 then
+    String.init (String.length (int_to_str_aux (abs i0) "" ) + 1) (fun i ->
+      if i = 0 then '-'
+      else String.get (int_to_str_aux (abs i0) "") (i - 1)
+    )
+  else
+    int_to_str_aux i0 ""
