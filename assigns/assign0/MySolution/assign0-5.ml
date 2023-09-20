20eda0fe1fe761951cad4ba8e7996cb97294f2f5
keith <keithy0157@gmail.com>
2023-09-19 21:12:11 -0400

    fixed up the files in 5

--- assigns/assign0/MySolution/assign0-5.ml
+++ assigns/assign0/MySolution/assign0-5.ml
@@ -9,13 +9,9 @@ or your solution is disqualified.
 
 #use "./..assign0.ml"
 
-let stringrev(cs: string): string =
-  let len = string_length cs in
-  (* calculates the length of the input string cs.*)
-
-  let result = string_init len (fun i ->
-    (*creates an empty string result of the same length as cs*)
-    string_get cs (len - 1 - i)
+let stringrev (cs: string): string =
+  let length = String.length cs in
+  let result = String.init length (fun i ->
+    String.get cs (length - 1 - i)
   ) in
-
   result
