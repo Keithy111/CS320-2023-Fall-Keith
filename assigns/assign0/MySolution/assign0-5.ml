96bb433127212a79d8e2ef6c824b5ea6c4b8f00f
keith <keithy0157@gmail.com>
2023-09-19 21:07:15 -0400

    fixing #5

--- assigns/assign0/MySolution/assign0-5.ml
+++ assigns/assign0/MySolution/assign0-5.ml
@@ -7,11 +7,7 @@ Note that you are not allowed to use string concatenation
 or your solution is disqualified.
 *)
 
-let string_init = String.init;;
-(*function is used to generate the reversed string by fetching characters from cs in reverse order and placing them in result*)
-let string_length = String.length;;
-let string_get = String.get;;
-(*used to retrieve the character at a specific index within a given string*)
+#use "./..assign0.ml"
 
 let stringrev(cs: string): string =
   let len = string_length cs in
