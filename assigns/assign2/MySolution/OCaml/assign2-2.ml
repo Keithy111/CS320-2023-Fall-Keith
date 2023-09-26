27fe3cd6f48354a4cd86fa9b91547668fee6eb0e
keith <keithy0157@gmail.com>
2023-09-26 03:21:02 -0400

    added the mylist_foreach and mylist_rforeach.

--- assigns/assign2/MySolution/OCaml/assign2-2.ml
+++ assigns/assign2/MySolution/OCaml/assign2-2.ml
@@ -24,6 +24,34 @@ type 'a mylist =
 (* ****** ****** *)
 exception MySubscript;;
 (* ****** ****** *)
+let rec
+mylist_foreach
+(xs: 'a mylist)
+(work: 'a -> unit): unit =
+match xs with
+| MyNil -> ()
+| MyCons(x1, xs) ->
+  (work(x1); mylist_foreach(xs)(work))
+| MySnoc(xs, x1) ->
+  (mylist_foreach(xs)(work); work(x1))
+| MyReverse(xs) -> mylist_rforeach(xs)(work)
+| MyAppend2(xs1, xs2) ->
+  (mylist_foreach(xs1)(work); mylist_foreach(xs2)(work))
+
+and
+mylist_rforeach
+(xs: 'a mylist)
+(work: 'a -> unit): unit =
+match xs with
+| MyNil -> ()
+| MyCons(x1, xs) ->
+  (mylist_rforeach(xs)(work); work(x1))
+| MySnoc(xs, x1) ->
+  (work(x1); mylist_rforeach(xs)(work))
+| MyReverse(xs) -> mylist_foreach(xs)(work)
+| MyAppend2(xs1, xs2) ->
+  (mylist_rforeach(xs2)(work); mylist_rforeach(xs1)(work))
+;;
 
 let rec mylist_length (xs: 'a mylist): int =
   match xs with
