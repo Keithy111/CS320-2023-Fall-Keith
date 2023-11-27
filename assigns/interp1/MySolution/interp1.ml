825ada8214ac49fdb5e94d3b89ac17ee99c8ac2e
keith <keithy0157@gmail.com>
2023-11-27 00:51:41 -0500

    first attempt at the test cases

--- assigns/interp1/MySolution/interp1.ml
+++ assigns/interp1/MySolution/interp1.ml
@@ -11,5 +11,83 @@ Notes:
 2. You may NOT use OCaml standard library functions directly.
 
 *)
+type config = {
+  stack: value list;
+  trace: string list;
+  program: command list;
+}
 
-let interp (s : string) : string list option = (* YOUR CODE *)
+and command =
+  | Push of int
+  | Add
+  | Sub
+  | Mul
+  | Div
+  | Rem
+  | Neg
+  | Swap
+  | Pop
+  | Quit
+  | True
+  | False
+  | Not
+  | Equal
+  | Lt
+  | Gt
+
+and value =
+  | Int of int
+  | Bool of bool
+
+let rec parse_prog (tokens: string list) : command list option =
+  let p = many' parse_command in
+  match string_parse p (string_concat_list tokens) with
+  | Some (cmds, _) -> Some cmds
+  | None -> None
+
+and parse_command () : command parser =
+  (keyword "push" >> whitespaces1 >> natural >|= fun n -> Push n)
+  <|> (keyword "add" >| Add)
+  <|> (keyword "sub" >| Sub)
+  <|> (keyword "mul" >| Mul)
+  <|> (keyword "div" >| Div)
+  <|> (keyword "rem" >| Rem)
+  <|> (keyword "neg" >| Neg)
+  <|> (keyword "swap" >| Swap)
+  <|> (keyword "pop" >| Pop)
+  <|> (keyword "quit" >| Quit)
+  <|> (keyword "true" >| True)
+  <|> (keyword "false" >| False)
+  <|> (keyword "not" >| Not)
+  <|> (keyword "equal" >| Equal)
+  <|> (keyword "lt" >| Lt)
+  <|> (keyword "gt" >| Gt)
+
+let initial_config (commands: command list) : config =
+  { stack = []; trace = []; program = commands }
+
+let interp_step (config: config) : config option =
+  match config.program, config.stack with
+  | [], _ -> Some { config with trace = "Success" :: config.trace; stack = []; program = [] }
+  | Quit :: _, _ -> Some { config with trace = "Success" :: config.trace; stack = []; program = [] }
+  | Push n :: rest, _ -> interp_step { config with stack = Int n :: config.stack; program = rest }
+  | Pop :: rest, Int _ :: stack' -> interp_step { config with stack = stack'; program = rest }
+  | Pop :: _, _ -> Some { config with trace = "Panic" :: config.trace; stack = []; program = [] }
+  | Add :: rest, Int y :: Int x :: stack' -> interp_step { config with stack = Int (x + y) :: stack'; program = rest }
+  | Add :: _, Int _ :: _ -> Some { config with trace = "Panic" :: config.trace; stack = []; program = [] }
+  | Add :: _, _ -> Some { config with trace = "Panic" :: config.trace; stack = []; program = [] }
+
+  | _ :: _, [] -> Some { config with trace = "Panic" :: config.trace; stack = []; program = [] }
+  | _ -> Some { config with trace = "Panic" :: config.trace; stack = []; program = [] }
+
+
+let interp (s : string) : string list option = 
+   let tokens = String.split_on_char ' ' program in
+   match parse_prog tokens with
+   | Some commands ->
+     let initial_configuration = initial_config commands in
+     (match interp_step initial_configuration with
+      | Some final_configuration ->
+        Some (final_configuration.trace |> foreach_to_listize(string_foreach))
+      | None -> None)
+   | None -> None
